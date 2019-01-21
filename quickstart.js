9f28dfeef45c53f2652241c20ea703a1e4b69bbf
Dwyane Lin <dw3lin@gmail.com>
2019-01-21 10:36:45 +0800

    modify youtube authentication dir

--- quickstart.js
+++ quickstart.js
@@ -6,8 +6,7 @@ var OAuth2=google.auth.OAuth2;
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/wakeHerUp.json
 var SCOPES=['https://www.googleapis.com/auth/youtube.readonly'];
-var TOKEN_DIR=(process.env.HOME || process.env.HOMEPATH ||
-		process.env.USERPROFILE)+'/.credentials/';
+//var TOKEN_DIR=(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE)+'/.credentials/';
 //var TOKEN_PATH=TOKEN_DIR+'wakeHerUp.json';
 var TOKEN_PATH='wakeHerUp.json';
 
@@ -84,14 +83,14 @@ function getNewToken(oauth2Client, callback){
  * @param {Object} token The token to store to disk.
  */
 function storeToken(token){
-	try{
+	/*try{
 		fs.mkdirSync(TOKEN_DIR);
 	}
 	catch(err){
 		if(err.code != 'EEXIST'){
 			throw err;
 		}
-	}
+	}*/
 	fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err)=>{
 		if(err) throw err;
 		console.log('Token stored to '+TOKEN_PATH);
